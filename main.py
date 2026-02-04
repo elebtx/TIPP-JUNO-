99d267d4a5f8b7c4218f6d8df97903c658324652
elebtx <eleanaberteaux125@gmail.com>
2026-02-04 10:15:25 +0100

    Enhance JUNO simulation with event counting and resolution
    Added total events calculation and resolution application for oscillation spectra.

--- main.py
+++ main.py
@@ -18,6 +18,8 @@ reactors = [
 flux_total_no_osc = np.zeros_like(energies)
 flux_total_with_osc_NO = np.zeros_like(energies)
 flux_total_with_osc_IO = np.zeros_like(energies)
+total_events_no_osc = 0
+total_events_osc_NO = 0
 
 print("Démarrage de la simulation JUNO...")
 
@@ -40,23 +42,41 @@ for core in reactors:
     flux_total_with_osc_NO += flux_pure * prob_survie_NO
     flux_total_with_osc_IO += flux_pure * prob_survie_IO
 
+    # Calcul des spectres par réacteur
+    spec_pure_core = flux_pure * sigma
+    spec_NO_core = flux_pure * prob_survie_NO * sigma
+    
+    # Somme des événements par jour pour ce réacteur
+    total_events_no_osc += phys.calculate_events_per_day(energies, spec_pure_core)
+    total_events_osc_NO += phys.calculate_events_per_day(energies, spec_NO_core)
+
 
 spec_no_osc = flux_total_no_osc * sigma
 spec_oscNO = flux_total_with_osc_NO * sigma
 spec_oscIO = flux_total_with_osc_IO * sigma 
 
+print(f"Nombre d'événements détecté par jour  : {total_events_osc_NO:.2f}")
 
+# Application de la résolution
+spec_oscNO_res = phys.energy_resolution(energies, spec_oscNO)
+spec_oscIO_res = phys.energy_resolution(energies, spec_oscIO)
+
+# Tracé du spectre après oscillation avec résolution
 plt.figure(figsize=(10, 6))
 
-plt.plot(energies, spec_no_osc, 'black', label="Sans Oscillation", alpha=0.5)
-plt.plot(energies, spec_oscNO, 'blue', linewidth=1, label="Avec Oscillation (NO)")
-plt.plot(energies, spec_oscIO, 'red', linewidth=1, label="Avec Oscillation (IO)")
+#plt.plot(energies, spec_no_osc, 'black', label="Sans Oscillation", alpha=0.5)
+plt.plot(energies, spec_oscNO, 'blue', linewidth=1, label="NO sans Résolution")
+plt.plot(energies, spec_oscIO, 'red', linewidth=1, label="IO sans Résolution")
+plt.plot(energies, spec_oscNO_res, '--', label="NO avec Résolution (3%)", alpha=0.8)
+plt.plot(energies, spec_oscIO_res, '--', label="IO avec Résolution (3%)", alpha=0.8)
 
 
-plt.title("Spectre JUNO : Effet des Oscillations ", fontsize=14)
+plt.title("Spectre JUNO", fontsize=14)
 plt.xlabel("Energie (MeV)")
 plt.ylabel("Evénements attendus (u.a.)")
 plt.legend()
 plt.grid(True, alpha=0.3)
 plt.tight_layout()
 plt.show()
+
+
